5eea102710fec9294b1d8df40e040515a985d3ac
kerrymuniz <kerrym877@gmail.com>
2023-08-25 17:47:47 -0300

    alteracao

--- index.php
+++ index.php
@@ -5,18 +5,72 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true //definir detalhes do erro;
-    ]
-]);
+$app = new \Slim\App(
+    ['settings' => [
+        'displayErrorDetails' => true
+    ]]
+);
 
-/* Container dependency injection */
+/* Tipos de respostas
+cabeçalho, texto, Json, XML
+*/
+
+$app->get('/header', function(Request $request, Response $response) {
+
+    $response->write('Esse é um retorno header');
+    return $response->withHeader('allow', 'PUT') //definir informações para o cabeçalho;
+             ->withAddedHeader('Content-Length', 10);
+
+});
+
+$app->get('/json', function(Request $request, Response $response) {
+
+    return $response->withJson([
+        "nome" => "Kerry Muniz",
+        "endereco" => "Endereco qualquer"
+    ]);
+
+});
+
+$app->get('/xml', function(Request $request, Response $response) {
+
+    $xml = file_get_contents('arquivo.xml'); //lendo o arquivo e armazenando no xml;
+    $response->write($xml);
+
+            //convertendo para um conteúdo xml e exibindo no browser;
+    return $response->withHeader('Content-Type', 'application/xml');
+
+});
+
+/* middleware */ //adicionam camadas de execução de códigos dentro da aplicação;
+
+$app->add( function() {
+    
+});
+
+$app->get('/usuarios', function(Request $request, Response $response) {
+
+    $response->write('Ação principal usuarios');
+
+});
+
+$app->get('/postagens', function(Request $request, Response $response) {
+
+    $response->write('Ação principal postagens');
+
+});
+
+
+$app->run();
+
+
+
+/* Container dependency injection
 class Servico {
     
-}
+}*/
 
-/* Container Pimple */
+/* Container Pimple
 $container = $app->getContainer(); //método que recupera o container do slim;
 $container['servico'] = function() {
     return new Servico;
@@ -27,12 +81,16 @@ $app->get('/servico', function(Request $request, Response $response) {
     $servico = $this->get('servico');
     var_dump($servico);
 
-});
+});*/
 
-/* Controllers como serviço */
-$app->get('/usuario', '\MyApp\controllers\Home:index'); //essa estrutura recebe (Classe:metodo);
+/* Controllers como serviço 
+$container = $app->getContainer(); //método que recupera o container do slim;
+$container['Home'] = function() {
+    return new MyApp\controllers\Home(new MyApp\View);
+};
+$app->get('/usuario', 'Home:index' ); //essa estrutura recebe (Classe:metodo);
 
 
-$app->run();
+$app->run();*/
 
 ?>
--- src/MyApp/controllers/Home.php
+++ src/MyApp/controllers/Home.php
@@ -4,7 +4,17 @@ namespace MyApp\controllers;
 
 class Home {
 
+    //protected $container;
+    protected $view;
+
+    public function __construct($view) {
+        $this->view = $view;
+    }
+
     public function index($request, $response) {
+
+        //$view = $this->container->get('View');
+        var_dump($this->view);
         return $response->write('testando a página');
     }
 
